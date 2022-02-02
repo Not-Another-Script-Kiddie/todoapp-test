afd73ff17c823b4074ced63d04e253f59c44b732
Joshua Empson <j.empson@livelinktechnology.net>
2022-02-02 17:36:01 +0000

    Basic pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController    ## defining classes = CamelCase
+  def home                                       ## defining methods = snake_case
+    ## Nothing here!
+  end
+
+  def about
+  end
+  
+  def help
+  end
+end
