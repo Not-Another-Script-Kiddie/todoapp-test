6206a8a9820698189324ab68dbbe6f28b3b7e842
Joshua Empson <j.empson@livelinktechnology.net>
2022-02-03 14:23:02 +0000

    More Front-end CRUD

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,24 @@ class TodosController < ApplicationController  ## Again basing this on the maste
     end
   end
 
+  def edit
+    @todo = Todo.find(params[:id])         ## find Todo to edit from the ID in browser path
+  end
+
+  def update
+    @todo = Todo.find(params[:id])         ## find the item
+    if @todo.update(todo_params)           ## update DB entry
+      flash[:notice] = 'Your To-Do item was successfully updated!'
+      redirect_to todo_path(@todo)         ## redirect to show view if successful
+    else
+      render 'edit'                        ## show same form w/ errs if update unsuccessful
+    end
+  end
+
+  def index
+    @todos = Todo.all                           ## simply grab all of the Todo items
+  end
+
   private                                                ## THIS CONTROLLER ONLY
     def todo_params
       params.require(:todo).permit(:name, :description)  ## whitelisting params fields
