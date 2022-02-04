5f2b6cf5e76ea1a27636dbd657d3c774f1c10d45
Joshua Empson <j.empson@livelinktechnology.net>
2022-02-04 17:13:57 +0000

    Form styling with outcome messages

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,12 @@
 class Todo < ApplicationRecord      ## will be ActiveRecord::Base in older Rails versions
   validates :name, presence: true, length: { maximum: 200 }
+
+  def length_val(field, minmax)  ## pass field name and which value i.e. :name, :maximum)
+    length_validators = _validators[field].select do |e|
+      e.is_a? ActiveRecord::Validations::LengthValidator
+    end
+    return nil if length_validators.empty?
+    length_validators.first.options[minmax]
+  end
+
 end
